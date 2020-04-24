a179e496fbed4f1492749534b7e7f112ab9ee9a3
Andrew Eichenlaub <andreweichenlaub@Andrews-MacBook-Pro-5.local>
2020-04-24 13:58:21 -0500

    updated contact form for netlify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,7 @@ export default class Contact extends Component {
 		return (
 			<FormContainer>
 				<form name="contact" method="POST" data-netlify="true">
+					<input type="hidden" name="form-name" value="contact" />
 	  				<p>
 	    				<label>Your Name: <input type="text" name="name" /></label>   
 	 				</p>
